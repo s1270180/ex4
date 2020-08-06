664db2b8e2ddfe41b5443482796695536d88f528
Makoto Suzuki <makoto@Air.local>
2020-08-06 18:15:20 +0900

    HoT: Initial revision

---
+++ HoT.java
@@ -0,0 +1,23 @@
+public class Hot{
+    public static void main(String args[]){
+	int head = 0;
+	int i =0;	
+	System.out.println("Tossing a coin...");
+	for(i=0;i<3;i++){
+	    int coin = (int)(Math.random()*10);
+			  
+	    if(coin > 4){
+		System.out.println("Round " +(i+1)+ ": Heads");
+		head++;
+	    
+	    }
+	    else{
+		System.out.println("Round " +(i+1)+": Tails");
+	    }
+	
+	   
+	}
+	System.out.println("Heads: " +head+ ", Tails: " +(3-head));
+    }
+}
+		  
