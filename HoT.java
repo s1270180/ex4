9d87ce0a35201ab37802efd7357fb632f09720c0
Makoto Suzuki <makoto@Air.local>
2020-08-06 18:44:48 +0900

    User name added

--- HoT.java
+++ HoT.java
@@ -1,7 +1,29 @@
+import java.io.*;
+
 public class Hot{
     public static void main(String args[]){
+
+	try {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+						   
+	    System.out.print("Who are you? ");
+	    System.out.print(">");
+	    String s = br.readLine();
+					       
+	    System.out.println("Hello, " + s+"!");
+					       
+	    br.close();
+	} catch (IOException e)
+	    {
+
+		e.printStackTrace();
+
+	    }
+    
+    
 	int head = 0;
-	int i =0;	
+	int i =0;
+	
 	System.out.println("Tossing a coin...");
 	for(i=0;i<3;i++){
 	    int coin = (int)(Math.random()*10);
